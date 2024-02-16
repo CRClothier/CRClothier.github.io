c9afb560b6cba0826e75a899dbb4f570c41f22c3
Chris Clothier <c.r.clothier@gmail.com>
2024-02-16 16:25:48 +0000

    Create hero section

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ function showMenu(){
     menuBar.classList.toggle('is-active');
     menuList.classList.toggle('is-active');
 }
+
+const linkText = document.getElementById('link-text');
+const userLanguage = navigator.language || navigator.userLanguage; // Fallback for compatibility
+
+// Check if British English is preferred
+if (userLanguage.startsWith('en-GB')) {
+  linkText.textContent = 'CV';
+} else {
+  linkText.textContent = 'Resume';
+}
